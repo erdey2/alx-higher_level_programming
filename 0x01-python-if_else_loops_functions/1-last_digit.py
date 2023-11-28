4defd2cdc023a0c39adeb6eab0a4a2808c79a4bf
Erdey <erdeysyoum@gmail.com>
2023-11-28 01:19:13 -0800

    extract last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = number % 10
+if number < 0:
+    lastdigit = (number * -1) % 10
+else:
+    lastdigit = number % 10
 if (lastdigit > 5):
     message = "and is greater than 5"
-elif(lastdigit == 0):
+elif (lastdigit == 0):
     message = "and is 0"
 else:
     message = "and is less than 6 and not zero"
