126618dd0baf15f29a05269144b194fa38ddd582
Erdey <erdeysyoum@gmail.com>
2023-11-28 07:33:11 -0800

    upper case checker

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     value = ord(str)
-    if value >= 65 and value <= 91:
-        return True
-    return False
+    if value >= 97 and value <= 122:
+        value = value - 32
+    return value
+
