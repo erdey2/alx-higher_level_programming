a9169e1ae96cee7c511e8b2c3ded8891f6b6aa84
Erdey <erdeysyoum@gmail.com>
2023-11-28 23:43:39 -0800

    changes string to upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        value = ord(str)
+        value = ord(str[i])
         if value >= 97 and value <= 122:
             i = chr(ord(i) - 32)
             print("{:s}".format(i), end="")
