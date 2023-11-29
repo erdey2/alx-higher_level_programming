7f3233647d9b2ba8e702d3a705c478715f4cb3d0
Erdey <erdeysyoum@gmail.com>
2023-11-28 22:11:16 -0800

    display fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(100):
+    for i in range(1:100):
         if i % 3 == 0:
             print("Fizz ", end='')
         elif i % 5 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz ", end='')
         else:
-            print(i, end='')
+            print(i, end=' ')
