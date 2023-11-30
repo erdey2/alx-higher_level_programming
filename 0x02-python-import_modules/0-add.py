95949651c8b578dcc0dd4f1a83a4e6bda52447c4
Erdey <erdeysyoum@gmail.com>
2023-11-30 03:37:47 -0800

    README.md added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import add_0
 import sys
+
+
 def main():
     a = 1
     b = 2
     res = add_0.add(a, b)
     print('{:d} + {:d} = {:d}'.format(a, b, res))
+
 
 if __name__ == '__main__':
     main()
