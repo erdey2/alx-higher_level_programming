a0936d469458c277c2bc24a7e28b6d8b9cace976
Erdey <erdeysyoum@gmail.com>
2023-11-30 04:15:11 -0800

    simple Calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    summ = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
+    print('{:d} + {:d} = {:d}'.format(a, b, summ))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul))
+    print('{:d} / {:d} = {:d}'.format(a, b, div))
