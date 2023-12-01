03639894a45823a910fbbd6254acfa2cfd51a559
Erdey <erdeysyoum@gmail.com>
2023-12-01 00:00:25 -0800

    simple calculator 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,19 +8,19 @@ if (__name__ == '__main__'):
         print('Usage: {} {} {} {}'.format(argv[0], '<a>', 'operator', '<b>'))
         exit(1)
     op = argv[2]
-    if ((op == '+') or (op == '-') or (op == '*') or (op == '/')):
+    if ((op == '+') or (op == '-') or (op == '/') or (op == '*')):
         if (op == '+'):
             summ = add(int(argv[1]), int(argv[3]))
             print('{:s} + {:s} = {:d}'.format(argv[1], argv[3], summ))
         elif (op == '-'):
             sub = sub(int(argv[1]), int(argv[3]))
             print('{:s} - {:s} = {:d}'.format(argv[1], argv[3], sub))
-        elif (op == '*'):
-            mul = mul(int(argv[1]), int(argv[3]))
-            print('{:s} * {:s} = {:d}'.format(argv[1], argv[3], mul))
         elif (op == '/'):
             div = div(int(argv[1]), int(argv[3]))
             print('{:s} / {:s} = {:d}'.format(argv[1], argv[3], div))
+        else:
+            mul = mul(int(argv[1]), int(argv[3]))
+            print('{:s} * {:s} = {:d}'.format(argv[1], argv[3], mul))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
