53d164700ad334cb62c1b2bae64f0842b2a4677e
Erdey <erdeysyoum@gmail.com>
2023-12-02 02:59:38 -0800

    get element at a given index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,7 @@ def element_at(my_list, idx):
     size = len(my_list)
     if (idx < 0):
         return None
-    elif (idx > size):
+    elif (idx >= size):
         return None
     else:
-        result = print('{:d}'.format(my_list[idx]))
-        return result
+        return my_list[idx]
