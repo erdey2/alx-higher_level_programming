bd2b7027de24f18b498b412483ccd47cb895221a
Erdey <erdeysyoum@gmail.com>
2023-12-06 01:23:04 -0800

    Add unique elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    if (len(my_list) != 0 and len(search) != 0 and len(replace) != 0):
-        for i in range(len(my_list)):
-            if search == my_list[i]:
-                my_list[i] = replace
-    return my_list
+def uniq_add(my_list=[]):
+    sum = 0
+    unique = set(my_list)
+    my_list2 = list(unique)
+    for i in range(len(my_list2)):
+        sum += my_list2[i]
+    return sum
