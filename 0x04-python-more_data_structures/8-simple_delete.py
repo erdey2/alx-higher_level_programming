9696a8618cad2cd5e42f993e3398fe806bbd97f6
Erdey <erdeysyoum@gmail.com>
2023-12-06 04:17:04 -0800

    Delete a name value pair from a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    for i in a_dictionary(keys()):
+        if key == i:
+            del a_dictionary[key]
     return a_dictionary
