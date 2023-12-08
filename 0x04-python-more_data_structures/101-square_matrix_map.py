13bac811c28ea1358e1525fe7c5b711b25daf636
Erdey <erdeysyoum@gmail.com>
2023-12-08 07:48:13 -0800

    square a matrix using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    list2 = list(map(lambda row :list(map(lambda element: (element ** 2), row)), matrix))
+    list2 = """list(map(lambda row:
+    list(map(lambda element: (element ** 2)), matrix))"""
     return list2
