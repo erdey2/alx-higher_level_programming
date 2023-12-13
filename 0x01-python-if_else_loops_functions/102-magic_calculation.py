bb13928cb2f38181a00c5759e9d60f55d56d7c49
Erdey <erdeysyoum@gmail.com>
2023-12-13 01:25:30 -0800

    bytecode to python

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif b > c:
+        return a
+    elif True:
+        return a + b
+    else
+        return b - c
+
