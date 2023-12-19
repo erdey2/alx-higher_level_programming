fb42c69db35ab9a87c8bcee06db905b34125d9e0
Erdey <erdeysyoum@gmail.com>
2023-12-19 03:50:11 -0800

    a square with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ a square class """
+    def __init__(self, size):
+        """ this is a constructor (init) method
+        Parameter:
+        - __size: the size of the square.
+        """
+        self.__size = size
