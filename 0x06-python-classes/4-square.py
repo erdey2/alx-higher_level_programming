538c8fc54f9a6670970cf9819b92b907f01290a3
Erdey <erdeysyoum@gmail.com>
2023-12-19 05:28:16 -0800

    square with getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" a square class """
+
+
+class Square:
+    """ this is a constructor (init) method
+        Parameter:
+        - __size: the size of the square.
+        """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+
+    @property
+    def size(self):
+        return self.__size
+
+
+    @property
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    """ calculate and display the area """
+    def area(self):
+        return self.__size * self.__size
