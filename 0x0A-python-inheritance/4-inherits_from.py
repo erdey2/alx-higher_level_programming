683810249a184d9f209e2abd642fa92150827957
Erdey <erdeysyoum@gmail.com>
2024-01-08 05:55:42 -0800

    checks class hierarchy

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Checks for subclass."""
 
+
 def inherits_from(obj, a_class):
     """Check for subclass of a given object.
     args:
@@ -8,4 +9,5 @@ def inherits_from(obj, a_class):
     a_class: the class
     Return: true if it is a subclass or false otherwise
     """
+
     return isinstance(obj, a_class)
