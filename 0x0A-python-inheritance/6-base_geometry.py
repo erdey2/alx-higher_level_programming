6879d658eaed3df2a6d6f661f2f88f4798a708e4
Erdey <erdeysyoum@gmail.com>
2024-01-08 06:04:27 -0800

    Base geometry with a public unimplemented method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Implement a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """An empty BaseGeometry class """
+    def area(self):
+        raise Exception('area() is not implemented')
