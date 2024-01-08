79c00cda51dc2883c5b9fe4ed3545cd225390796
Erdey <erdeysyoum@gmail.com>
2024-01-08 06:30:30 -0800

    base gemetry with exception

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,6 +6,7 @@ class BaseGeometry:
     """An empty BaseGeometry class """
     def area(self):
         raise Exception('area() is not implemented')
+
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f'{name} must be an integer')
