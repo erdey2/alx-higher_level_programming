53f17acd4e48d5f73b23db8f308ca0ec8400c17c
Erdey <erdeysyoum@gmail.com>
2024-01-08 03:20:22 -0800

    check object's instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """check object for equality."""
 
+
 def is_same_class(obj, a_class):
     """return true if an object is an instance otherwise fasle."""
     if isinstance(obj, a_class):
