d7f8c521c3ffa50ab6ae3fea5734fb78dd07077a
Erdey <erdeysyoum@gmail.com>
2024-01-09 01:16:41 -0800

    Read and display a file content

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Read a file content."""
-    with(open(filename, 'r')) as file:
+    with(open(filename, encoding="utf-8")) as file:
         print(file.read(), end="")
     file.close()
