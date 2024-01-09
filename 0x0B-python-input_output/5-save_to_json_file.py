bc62380d62ff0225ef82493cbc8ccfcb58420272
Erdey <erdeysyoum@gmail.com>
2024-01-09 05:09:58 -0800

    Json object to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Save object to file."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write json to a file."""
+    with(open(filename, 'w', encoding = 'utf-8')) as file:
+        return json.dump(my_obj, file)
