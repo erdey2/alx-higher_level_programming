34c632c4f9869cefec5c928b18059678dccd1e90
Erdey <erdeysyoum@gmail.com>
2024-01-09 07:13:32 -0800

    convert user defined class to json

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,11 @@ class Student(object):
     age
 
     def __init__(self, first_name, last_name, age):
+        """initialize instance variables."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """convert object to json."""
         return self.__dict__.copy()
