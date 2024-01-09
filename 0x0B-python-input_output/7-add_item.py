8678aff3f7817cc5438ef80ba71e519b802528f6
Erdey <erdeysyoum@gmail.com>
2024-01-09 06:11:08 -0800

    add items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """add program args module"""
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
-
 import sys
 import os.path
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
 list1 = []
