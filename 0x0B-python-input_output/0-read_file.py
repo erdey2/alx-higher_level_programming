680d026301fbbe9999ce6e2eb390f64a283dd46d
Erdey <erdeysyoum@gmail.com>
2024-01-09 05:31:13 -0800

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """Read a file content."""
-    with(open(filename, encoding="utf-8")) as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end="")
+    file.close()
