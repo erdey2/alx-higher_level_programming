bb6d034f6e9d256b1d1793590c200f80a3efa5d5
Erdey <erdeysyoum@gmail.com>
2024-01-09 02:05:13 -0800

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Append a text module."""
+
+
+def append_write(filename="", text=""):
+    """ Append a text to a given string.
+    Args:
+        filename: the name of the file.
+        text: the string to be written.
+    Returns:
+        the number of characters appended
+    """
+    with(open(filename, 'a', encoding = "utf-8")) as file:
+        return file.write(text)
