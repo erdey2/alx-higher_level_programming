61dba0502a6f5aa27940d41fa4244c3a927e908f
Erdey <erdeysyoum@gmail.com>
2024-01-09 05:35:34 -0800

    append to file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,5 +10,5 @@ def append_write(filename="", text=""):
     Returns:
         the number of characters appended
     """
-    with(open(filename, 'a', encoding = "utf-8")) as file:
+    with (open(filename, 'a', encoding = "utf-8")) as file:
         return file.write(text)
