3e74a64dc5291f2296a904de02bc21cc532c8cbc
Erdey <erdeysyoum@gmail.com>
2024-01-10 05:23:07 -0800

    Print square of #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""print square module."""
+
+
+def print_square(size):
+    """A function that print a square of #."""
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise TypeError('size must be >= 0')
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
