73bac94708c67e28fc81bf3c200369ea8ebff77a
Erdey <erdeysyoum@gmail.com>
2024-01-10 04:39:53 -0800

    Display name test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,6 +11,7 @@ def say_my_name(first_name, last_name=""):
     msg = f'My name is {first_name} {last_name}'
     print(msg)
 
+
 if __name__ == '__main__':
     import doctest
     doctest.testfile('tests/3-say_my_name.txt')
