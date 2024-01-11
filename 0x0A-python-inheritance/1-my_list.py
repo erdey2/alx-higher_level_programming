f947d202461fe91837b638c4060b6b67147a6710
Erdey <erdeysyoum@gmail.com>
2024-01-10 22:49:56 -0800

    list sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """MyList class implementation."""
 
+
 class MyList(list):
     """print sorted list."""
     def print_sorted(self):
-        print(sorted(self))
+        """print list in asending order."""
+        copy_list = self[:]
+        copy_list.sort()
+        print(copy_list)
