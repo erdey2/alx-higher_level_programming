df3a44b8eddcb7a43c83680b82e33cd94c12a7ea
Erdey <erdeysyoum@gmail.com>
2024-01-11 08:07:20 -0800

    set attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,11 +1,27 @@
-#!/usr/bin/python3
-"""add attribute to object."""
-
-
 def add_attribute(obj, name, value):
-    """Implement add_attribute."""
-    if not hasattr(obj, "__dict__"):
-        raise TypeError("Can't add new attribute")
+  """
+  Adds a new attribute to an object if it doesn't already exist.
 
+  Args:
+    obj: The object to add the attribute to.
+    name: The name of the attribute to add.
+    value: The value of the attribute to add.
 
-setattr(obj, name, value)
+  Raises:
+    TypeError: If the object doesn't allow adding attributes.
+
+  Returns:
+    The modified object.
+  """
+
+  if hasattr(obj, "__dict__"):
+    # Use __dict__ for standard objects
+    obj.__dict__[name] = value
+  else:
+    # Check for specific methods to add attributes for custom classes
+    if hasattr(obj, "set_attribute"):
+      obj.set_attribute(name, value)  # Change this to the actual method name
+    else:
+      raise TypeError("Cannot add attributes to this object")
+
+  return obj
