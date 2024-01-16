535f338e259e03d7033a96cd37d4c23be93fa5a5
Erdey <erdeysyoum@gmail.com>
2024-01-16 04:11:46 -0800

    dict to JSON added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,7 +13,7 @@ class Base(object):
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         if not list_dictionaries:
