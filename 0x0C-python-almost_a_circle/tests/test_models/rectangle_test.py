d6b2bf6574264b09be70f203f423124790936601
Erdey <erdeysyoum@gmail.com>
2024-01-17 01:12:26 -0800

    Rectangle test

--- 0x0C-python-almost_a_circle/tests/test_models/rectangle_test.py
+++ 0x0C-python-almost_a_circle/tests/test_models/rectangle_test.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python3
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
     """Test rectangle."""
-    test_area_positive(self):
+    def test_area_positive(self):
         r1 = Rectangle(3, 2)
         self.assertEqual(r1, 6)
 
     
-    test_area_negative(self):
+    def test_area_negative(self):
         r2 = Rectangle(-3, -2)
         self.assertEqual(r2, 6)
 
-    test_area_empty(self):
+    def test_area_empty(self):
         r3 = Rectangle()
         self.assertEqual(r3, 0)
     
-    test_area_string(self):
+    def test_area_string(self):
         r4 = Rectangle('-3', 2)
         self.assertEqual(r4, '-3-3')
