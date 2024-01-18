34ec2d984540f629b163b38a2f3f66e4465c5e0e
Erdey <erdeysyoum@gmail.com>
2024-01-17 23:12:52 -0800

    base class updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,13 @@ class Base(object):
         if not json_string:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
