e5d98b094db4992f9dbc26eb7f929f5d19f675f6
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-14 03:45:07 -0800

    relation ship

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- create a table in a given database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS 'cities'
 (
