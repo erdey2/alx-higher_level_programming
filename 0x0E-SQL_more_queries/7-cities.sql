6a7a158b00d12393cc65fe85218c1d1f4213cd69
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-14 03:29:29 -0800

    create table in a given database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
 (
-	id INT UNIQUE INCREMENT primary key,
+	id INT UNIQUE AUTO_INCREMENT primary key,
 	name VARCHAR(256) NOT NULL
 );
