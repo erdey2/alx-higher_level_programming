79171c124e4dc5e7510ddbc073c3458fbd73164f
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-14 03:40:53 -0800

    relation ship

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- create a table in a given database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'states'
+CREATE TABLE IF NOT EXISTS cities
 (
-	id INT AUTO_INCREMENT primary key,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT FOREIGN KEY REFERENCES states(id) NOT NULL,
 	name VARCHAR(256) NOT NULL
 );
