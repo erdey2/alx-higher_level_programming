3536825a870169f078b10a50c904052e82a0f55c
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-14 20:55:03 -0800

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,5 @@
 SELECT title, genre_id 
 FROM tv_show_genres 
 RIGHT JOIN tv_shows 
-ON tv_show.id = tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY title, tv_show_genres.genre_id ASC;
