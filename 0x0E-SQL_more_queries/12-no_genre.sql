9cfe3145399235250c5d5a7c4d869bfb7c30082b
Erdey Syoum <erdeysyoum@gmail.com>
2024-02-14 22:21:36 -0800

    tv shows

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- no genre
 SELECT title, genre_id 
-FROM tv_shows LEFT JOIN tv_show_genre
-ON tv_shows.id = tv_show_genre.genre_id 
-WHERE tv_show_genre.genre_id IS NULL
-ORDER BY title, tv_show_genre.genre_id ASC;
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id 
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY title, tv_show_genres.genre_id ASC;
