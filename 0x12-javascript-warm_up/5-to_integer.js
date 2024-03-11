4b3b522a42cc04eec5b46a9adc914a0c7c52d347
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-11 04:27:24 -0700

    convert to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 const process = require('node:process');
-let args = process.argv
-if (args.length == 3)
-{
-	let number = Number(process.argv[2]);
-	if (number)
-	{
-		console.log(number);
-	}
-	else
-	{
-		console.log('Not a number');
-	}
+const args = process.argv;
+if (args.length === 3) {
+  const number = Number(process.argv[2]);
+  if (number) {
+    console.log(number);
+  } else {
+    console.log('Not a number');
+  }
 }
-
