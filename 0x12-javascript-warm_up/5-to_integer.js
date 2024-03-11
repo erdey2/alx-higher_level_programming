601ef398d220681bccd2c9f589791a333c85d5ad
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-11 04:46:25 -0700

    convert to number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,9 @@ const process = require('node:process');
 const args = process.argv;
 if (args.length === 3) {
   const number = Number(process.argv[2]);
-  if (number) {
-    console.log(number);
-  } else {
+  if (isNaN(number)) {
     console.log('Not a number');
+  } else {
+    console.log('My number: ' + number);
   }
 }
