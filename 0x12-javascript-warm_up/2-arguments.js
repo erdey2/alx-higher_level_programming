b543c0dd1db728c764a2fc69654b327693e69a3c
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-11 04:12:53 -0700

    multiple arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const process = require ('node:process');
-let args = process.argv;
-if (args.length < 3)
-{
-	console.log('No argument');
+const process = require('node:process');
+const args = process.argv;
+if (args.length < 3) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (args.length === 3)
-{
-	console.log('Argument found');
-}
-else
-	console.log('Arguments found');
