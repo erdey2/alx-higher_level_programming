21ce6ca1ee6e2ced80fd96809e2c43d0a66707aa
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-11 04:16:40 -0700

    check value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const process = require('node:process');
-let args = process.argv;
-if (args.length < 3)
-	console.log('No argument');
-else if (args.length == 3)
-	console.log(process.argv[2]);
+const args = process.argv;
+if (args.length < 3) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log(process.argv[2]);
+}
