8db4e8d3115a7a945d1db39835346dd0ff4bb1b7
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-21 06:03:38 -0700

    create database

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """
-class table mapper
+object relational mapping module
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 
 class State(Base):
-    """State is subclass of the Base"""
-    __tablename__ = 'states'
+    """
+    State is subclass of the Base
+
+    Attributes:
+        id: state id
+        name: state name
+    """
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
