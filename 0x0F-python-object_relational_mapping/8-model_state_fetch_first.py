04730405f0175ec0cf37fb97cf588973e24905da
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-21 06:16:44 -0700

    prints state object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Prints state object from the mysql database
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    # create an engine object that will interact with the mysql
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    Base.metadata.create_all(engine)
+    s_tate = session.query(State).order_by(State.id).first()
+
+    if s_state:
+        print("{}: {}".format(s_state.id, s_state.name))
+    else:
+        print("Nothing")
+    session.close()
