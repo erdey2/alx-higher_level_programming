a8e4ad3a17978ca3cd5141eb0b46267bbcdc3d53
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-29 07:37:58 -0700

    post data

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ import requests
 
 if __name__ == "__main__":
     value = {"email": argv[2]}
-    r = requests.post(argv[1], data = value)
+    r = requests.post(argv[1], data=value)
     print(r.text)
