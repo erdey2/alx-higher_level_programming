5f8bbcc1d58bd519e93d50c201e1e1e3bc16cc1b
Erdey Syoum <erdeysyoum@gmail.com>
2024-03-29 03:08:16 -0700

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ A module to fetch url"""
 from urllib import request
-with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+
+
+if __name__ = "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print('Body response:')
     print('\t- type: ', type(html))
