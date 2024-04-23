b06c9f24e6a7a31fae7b7674d772bc80a5e2fbab
erdey <erdeysyoum@gmail.com>
2024-04-23 09:21:42 -0700

    films length

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 const apiEnd = 'https://swapi-api.alx-tools.com/api/people/18';
 request(apiEnd, function (error, response, body) {
   if (error) { console.log(error); } else {
