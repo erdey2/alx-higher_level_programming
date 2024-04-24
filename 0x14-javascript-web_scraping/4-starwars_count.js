208eee90a7b2008092d82789bfcadbf284202e54
erdey <erdeysyoum@gmail.com>
2024-04-24 00:45:28 -0700

    count films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 request(process.argv[2], function (error, response, body) {
-  if (error) { console.log(error); } else {
-    let films = JSON.parse(body).films
-    console.log(films.reduce((count, movie) => {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
